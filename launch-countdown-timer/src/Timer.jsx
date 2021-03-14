f8c2706bff71318e4939370dc0676fdbd53a2213
Thy Christiansen <thyvu0411@gmail.com>
2021-03-13 23:41:20 -0600

    basic frame for numbers

--- launch-countdown-timer/src/Timer.jsx
+++ launch-countdown-timer/src/Timer.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import './styles/style.css';
+
 
 const Timer = (props) => {
-    const {initialDay=10,initialHour=23,initialMinute = 60,initialSeconds = 0} = props;
-    const [ days, setDays ] = useState(initialDay);
-    const [ hours, setHours] = useState(initialHour);
-    const [ minutes, setMinutes ] = useState(initialMinute);
-    const [seconds, setSeconds ] =  useState(initialSeconds);
-    useEffect(()=>{
-    let myInterval = setInterval(() => {
+    const { initialDay = 10, initialHour = 23, initialMinute = 60, initialSeconds = 0 } = props;
+    const [days, setDays] = useState(initialDay);
+    const [hours, setHours] = useState(initialHour);
+    const [minutes, setMinutes] = useState(initialMinute);
+    const [seconds, setSeconds] = useState(initialSeconds);
+    useEffect(() => {
+        let myInterval = setInterval(() => {
             if (seconds > 0) {
                 setSeconds(seconds - 1);
             }
@@ -19,7 +21,7 @@ const Timer = (props) => {
                     setMinutes(minutes - 1);
                     setSeconds(59);
                 }
-            } 
+            }
             if (minutes === 0) {
                 if (hours === 0) {
                     clearInterval(myInterval)
@@ -37,17 +39,40 @@ const Timer = (props) => {
                 }
             }
         }, 1000)
-        return ()=> {
+        return () => {
             clearInterval(myInterval);
-          };
+        };
     });
 
+    const number = (e) => {
+        return <div class="num-container">
+            <div class="num">
+                {e < 10 ?
+                    <>
+                        <div class="top">0{e}</div>
+                        <div class="bottom"><div class="shift">0{e}</div></div>
+                    </>
+                    :
+                    <>
+                        <div class="top">{e}</div>
+                        <div class="bottom"><div class="shift">{e}</div></div>
+                    </>
+                }
+            </div>
+        </div>
+    }
+
     return (
-        <div>
-        { minutes === 0 && seconds === 0
-            ? null
-            : <h1> {days}:{hours}:{minutes < 10 ?  `0${minutes}` : minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
-        }
+        <div className="counter">
+            { minutes === 0 && seconds === 0
+                ? null
+                : <div> {number(days)}
+            :{number(hours)}
+            :{number(minutes)}
+            :{number(seconds)}</div>
+            }
+
+
         </div>
     )
 }
